cf30ca7d0803427e66df0e412fa867e08d7982e4
LaurentArcos <laurent.arcos@gmail.com>
2023-03-20 10:44:43 +0100

    consignes commentées

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <main>
         <h1>Baby shark</h1>
         <div class="lyrics">
-            <?php 
+            <?php
             // Membres de la famille Requin
             $family = ['baby', 'mommy', 'daddy', 'grandma', 'grandpa'];
 
@@ -21,32 +21,32 @@
             <?php foreach($family as $member): ?>
 
                 <div class="verse member-<?php {echo"$member";} ?>" >
-                    
+
                     <?php for ($i = 0; $i<3; $i++):?>
                         <p>
                             <?php echo "$member shark doo doo doo doo doo"; ?>
                         </p>
-                    
-                    <?php endfor; ?>    
-                    
+
+                    <?php endfor; ?>
+
                     <p>
                         <?= $member ?> shark !
                     </p>
-                
-                </div>
-            
-            <?php endforeach; ?>
-               
 
-            <!-- Chaque couplet doit être représenté par une div comme celle-ci : -->
+                </div>
+
+            <?php endforeach; ?>
+
+
+            <!-- Chaque couplet doit être représenté par une div comme celle-ci : 
             <div class="verse member-NOM-REQUIN">
                 <p>Les paroles ici</p>
                 <p>Encore des paroles là</p>
             </div>
-
+            -->
         </div>
     </main>
-                    
+
 </body>
 </html>
 
